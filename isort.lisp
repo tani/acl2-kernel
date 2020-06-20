42e599bad9336fe22a2ec3c544487c8c6f598c63
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-20 19:33:38 +0900

    Add MEM

--- isort.lisp
+++ isort.lisp
@@ -1,6 +1,17 @@
 (IN-PACKAGE "ACL2")
+
+(DEFUN MEM (X Y)
+  (IF (MEMBER X Y) T NIL))
+
+(DEFUN DEL (X Y)
+    (IF (CONSP Y)
+        (IF (EQUAL X (CAR Y))
+            (CDR Y)
+            (CONS (CAR Y) (DEL X (CDR Y))))
+        Y))
+
 (DEFUN PERM (X Y)
     (IF (CONSP X)
-        (AND (MEMBER (CAR X) Y)
-             (PERM (CDR X) (REMOVE X (CAR X))))
+        (AND (MEM (CAR X) Y)
+             (PERM (CDR X) (DEL (CAR X) Y)))
         (NOT (CONSP Y))))
